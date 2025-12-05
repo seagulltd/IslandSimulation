0dad945d033993935f305ddd663c6fa90f1fe5ca
seagulltd <talyadukhovna7@icloud.com>
2025-12-05 14:37:06 +0300

    Update Predator.java

--- src/animals/Predator.java
+++ src/animals/Predator.java
@@ -1,1 +1,10 @@
+package animals;
 
+import java.util.Map;
+
+public abstract class Predator extends Animal {
+    public Predator(String name, double weight, int maxPerCell, int maxSpeed,
+                    double foodRequired, Map<String, Integer> foodProbabilities) {
+        super(name, weight, maxPerCell, maxSpeed, foodRequired, foodProbabilities);
+    }
+}
