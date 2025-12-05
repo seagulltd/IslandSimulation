aa80ccb4e463906dbaefd4f006588ac6b839db8b
seagulltd <talyadukhovna7@icloud.com>
2025-12-05 14:36:49 +0300

    Update Herbivore.java

--- src/animals/Herbivore.java
+++ src/animals/Herbivore.java
@@ -1,1 +1,15 @@
+package animals;
+
+import island.Island;
+import island.IslandCell;
+
+import java.util.Map;
+
+public abstract class Herbivore extends Animal {
+    public Herbivore(String name, double weight, int maxPerCell, int maxSpeed,
+                     double foodRequired, Map<String, Integer> foodProbabilities) {
+        super(name, weight, maxPerCell, maxSpeed, foodRequired, foodProbabilities);
+    }
+}
+
 
