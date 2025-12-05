340577ad86743649d103a8fef3efe4794d53bf3f
seagulltd <talyadukhovna7@icloud.com>
2025-12-05 14:33:58 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import island.Island;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start simulation!");
+        System.out.println("Симуляция запущена!");
+
+        Island island = new Island(5, 5);
+        island.startSimulation();
+
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {}
+
+        island.stopSimulation();
+        System.out.println("Симуляция завершена.");
     }
 }
